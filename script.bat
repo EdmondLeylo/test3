da395fd96de749d7c50a1e90ad044967b8b31db8
Edmond Leylo <edmondlaylo@gmail.com>
2015-04-08 08:49:36 +0300

    11

---
+++ script.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+
+set inputfile=file.txt
+
+set tempfile=%random%-%random%.tmp
+
+copy /y nul %tempfile%
+
+set line=0
+
+for /f "delims=:" %%v in ('findstr /n %1 file.txt') do set ln=%%v
+
+for /f "delims=" %%l in (%inputfile%) do (
+    set /a line+=1
+    if !line!==%ln% (
+        echo WORDS YOU REPLACE IT WITH>>%tempfile%
+    ) else (
+        echo %%l>>%tempfile%
+    )
+)
+
+del %inputfile%
+ren %tempfile% %inputfile%
+
+endlocal
